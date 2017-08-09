22cf634767b3debcde0f04edbf0612ffbc56a560
petervaro <hello@petervaro.com>
2017-08-09 11:14:44 +0100

    bash history cleaner added

---
+++ scripts/cleanhistory.py
@@ -0,0 +1,48 @@
+## INFO ##
+## INFO ##
+
+from subprocess import run
+from os.path    import expanduser
+
+EXCLUDE  = ('cd',
+            'clear',
+            'reset',
+            'hh',
+            'reboot',
+            'poweroff',
+            'shutdown',
+            'source',
+            'ls',
+            'startx',
+            'mv',
+            'rm',
+            'cp',
+            'cat',
+            'echo',
+            'printf',
+            'cloc',
+            'man',
+            'mkdir',
+            'touch')
+EXCLUDE += tuple('sudo {}'.format(cmd) for cmd in EXCLUDE)
+
+history = set()
+with open(expanduser('~/.bash_history'), 'r+') as file:
+    for line in file:
+        if not line:
+            continue
+        for exclude in EXCLUDE:
+            if line.lower().startswith(exclude):
+                break
+        else:
+            history.add(line)
+    file.seek(0)
+    file.write(''.join(sorted(history)))
+    file.truncate()
+
+# Clear history from memory
+run('history -c', shell=True)
+# Read history from file
+run('history -r', shell=True)
+# Write file back from memory
+run('history -w', shell=True)
